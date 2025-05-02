b62030546cf6590077a7dc5b4b012daf1690a3fb
c6z3h <chuazhihong@hotmail.com>
2025-05-02 17:07:04 +0800

    Update possible issue with @types/react version

--- next.config.js
+++ next.config.js
@@ -3,9 +3,6 @@ const nextConfig = {
   output: 'export', // enables static exports (compatible with GitHub pages)
   reactStrictMode: true,
   // swcMinify: true, // TODO remove this until we figure what it's for
-  eslint: {
-    ignoreDuringBuilds: true, // TODO keep this until Github Actions can correctly lint
-  }
 }
 
 module.exports = nextConfig
