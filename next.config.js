8b2414dbfbb45ab66a62b7f9d907bb044687f8fc
c6z3h <chuazhihong@hotmail.com>
2025-05-02 16:12:26 +0800

    Flatten file structure

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', // enables static exports (compatible with GitHub pages)
   reactStrictMode: true,
   swcMinify: true,
 }
